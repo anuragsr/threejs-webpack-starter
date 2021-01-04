5369e931583078d12b17dc1698562a8c77dc9506
Anurag Srivastava <anurag.131092@gmail.com>
2021-01-04 15:31:06 +0530

    Changed structure, updated webpack config

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,100 +1,79 @@
 const path = require('path');
-
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-    devtool: 'eval-cheap-module-source-map',
-    entry: './src/index.js',
-    devServer: {
-        disableHostCheck: true,
-        port: 8080,
-        contentBase: path.join(__dirname, "build"),
-        publicPath: '/'
-    },
-    node: {
-        fs: 'empty'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: 'babel-loader',
-                options: {
-                    presets: ['env']
-                }
-            },
-            {
-                test: /\.(scss|css)$/,
-                use: [
-                    {
-                        // creates style nodes from JS strings
-                        loader: "style-loader",
-                        options: {
-                            sourceMap: true
-                        }
-                    },
-                    {
-                        // translates CSS into CommonJS
-                        loader: "css-loader",
-                        options: {
-                            sourceMap: true
-                        }
-                    },
-                    {
-                        // compiles Sass to CSS
-                        loader: "sass-loader",
-                        options: {
-                            outputStyle: 'expanded',
-                            sourceMap: true,
-                            sourceMapContents: true
-                        }
-                    }
-                    // Please note we are not running postcss here
-                ]
-            }
-            ,
-            {
-                // Load all images as base64 encoding if they are smaller than 8192 bytes
-                test: /\.(png|jpg|gif)$/,
-                use: [
-                    {
-                        loader: 'url-loader',
-                        options: {
-                            // On development we want to see where the file is coming from, hence we preserve the [path]
-                            name: '[path][name].[ext]?hash=[hash:20]',
-                            limit: 8192
-                        }
-                    }
-                ]
-            }
-            ,
-            {
-                // Load all icons
-                test: /\.(eot|woff|woff2|svg|ttf|otf)([\?]?.*)$/,
-                use: [
-                    {
-                        loader: 'file-loader',
-                    }
-                ]
-            }
-            // ,
-            // {
-            //   test: /\.(obj|mtl|fbx)$/,
-            //   loader: 'file-loader'
-            // },
+  devtool: 'eval-cheap-module-source-map',
+  entry: './src/index.js',
+  devServer: {
+    historyApiFallback: true,
+    disableHostCheck: true,
+    port: 8080,
+    open: true,
+    contentBase: path.join(__dirname, "build"),
+    publicPath: '/',
+  },
+  node: { fs: 'empty' },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env'],
+          plugins: ['@babel/plugin-proposal-object-rest-spread']
+        }
+      },
+      {
+        test: /\.(css)$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.(scss)$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          { loader: 'css-loader', options: { url: false } },
+          'sass-loader'
         ],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './index.html',
-            inject: true
-        }),
-        new CopyWebpackPlugin({
-            patterns: [
-                { from: './src/assets', to: 'assets' }
-            ]
-        })
-    ]
+      },
+      {
+        // Load all images as base64 encoding if they are smaller than 8192 bytes
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            // On development we want to see where the file is coming from, hence we preserve the [path]
+            loader: 'url-loader',
+            options: { name: '[path][name].[ext]?hash=[hash:20]', limit: 8192 }
+          }
+        ]
+      },
+      {
+        // Load all icons
+        test: /\.(eot|woff|woff2|svg|ttf|otf)([\?]?.*)$/,
+        use: [
+          { loader: 'file-loader' }
+        ]
+      },
+      {
+        // HTML LOADER
+        // Reference: https://github.com/webpack/raw-loader
+        // Allow loading html through js
+        test: /\.html$/,
+        loader: 'raw-loader'
+      }
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      // Inject the js bundle at the end of the body of the given template
+      inject: 'body',
+    }),
+    new MiniCssExtractPlugin({ filename: "style.css" }),
+    new CopyWebpackPlugin({
+      patterns: [{from: __dirname + '/public'}]
+    }),
+  ]
 };
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,146 +1,105 @@
 const path = require('path');
 
-const FaviconsWebpackPlugin = require('favicons-webpack-plugin')
 const CleanWebpackPlugin = require('clean-webpack-plugin'); //installed via npm
 const CopyWebpackPlugin = require('copy-webpack-plugin'); //installed via npm
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const { BaseHrefWebpackPlugin } = require('base-href-webpack-plugin');
 
 const buildPath = path.resolve(__dirname, 'build');
 
 module.exports = {
-    devtool: 'source-map',
-    entry: './src/index.js',
-    output: {
-        filename: '[name].[hash:20].js',
-        path: buildPath
-    },
-    node: {
-        fs: 'empty'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: 'babel-loader',
-
-                options: {
-                    presets: ['env']
-                }
-            },
-            {
-                test: /\.(scss|css|sass)$/,
-                use: [
-                    {
-                        loader: MiniCssExtractPlugin.loader
-                    },
-                    {
-                        // translates CSS into CommonJS
-                        loader: 'css-loader',
-                        options: {
-                            sourceMap: true
-                        }
-                    },
-                    {
-                        // Runs compiled CSS through postcss for vendor prefixing
-                        loader: 'postcss-loader',
-                        options: {
-                            sourceMap: true
-                        }
-                    },
-                    {
-                        // compiles Sass to CSS
-                        loader: 'sass-loader',
-                        options: {
-                            outputStyle: 'expanded',
-                            sourceMap: true,
-                            sourceMapContents: true
-                        }
-                    }
-                ]
-            },
-            {
-                // Load all images as base64 encoding if they are smaller than 8192 bytes
-                test: /\.(png|jpg|gif)$/,
-                use: [
-                    {
-                        loader: 'url-loader',
-                        options: {
-                            name: '[name].[hash:20].[ext]',
-                            limit: 8192
-                        }
-                    }
-                ]
-            }
-            ,
-            {
-                // Load all icons
-                test: /\.(eot|woff|woff2|svg|ttf|otf)([\?]?.*)$/,
-                use: [
-                    {
-                        loader: 'file-loader',
-                    }
-                ]
-            }
+  // devtool: 'source-map',
+  entry: './src/index.js',
+  output: {
+    filename: '[name].[hash:20].js',
+    path: buildPath
+  },
+  node: { fs: 'empty' },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env'],
+          plugins: ['@babel/plugin-proposal-object-rest-spread']
+        }
+      },
+      {
+        test: /\.(css)$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.(scss|sass)$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            // translates CSS into CommonJS
+            loader: 'css-loader',
+            options: { url: false, sourceMap: false }
+          },
+          {
+            // Runs compiled CSS through postcss for vendor prefixing
+            loader: 'postcss-loader',
+            options: { sourceMap: false }
+          },
+          {
+            // compiles Sass to CSS
+            loader: 'sass-loader',
+            options: { sourceMap: false }
+          }
         ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './index.html',
-            // Inject the js bundle at the end of the body of the given template
-            inject: 'body',
-        }),
-        new CleanWebpackPlugin(buildPath),
-        // new FaviconsWebpackPlugin({
-            // Your source logo
-        //     logo: './src/assets/icon.png',
-            // The prefix for all image files (might be a folder or a name)
-        //     prefix: 'icons-[hash]/',
-            // Generate a cache file with control hashes and
-            // don't rebuild the favicons until those hashes change
-        //     persistentCache: true,
-            // Inject the html into the html-webpack-plugin
-        //     inject: true,
-        //     // favicon background color (see https://github.com/haydenbleasel/favicons#usage)
-        //     background: '#fff',
-        //     // favicon app title (see https://github.com/haydenbleasel/favicons#usage)
-        //     title: 'threejs-starter',
-
-        //     // which icons should be generated (see https://github.com/haydenbleasel/favicons#usage)
-        //     icons: {
-        //         android: true,
-        //         appleIcon: true,
-        //         appleStartup: true,
-        //         coast: false,
-        //         favicons: true,
-        //         firefox: true,
-        //         opengraph: false,
-        //         twitter: false,
-        //         yandex: false,
-        //         windows: false
-        //     }
-        new CopyWebpackPlugin({
-            patterns: [
-                { from: './src/assets', to: 'assets' }
-            ]
-        }),
-        new MiniCssExtractPlugin({
-            filename: 'styles.[contenthash].css'
-        }),
-        new OptimizeCssAssetsPlugin({
-            cssProcessor: require('cssnano'),
-            cssProcessorOptions: {
-                map: {
-                    inline: false,
-                },
-                discardComments: {
-                    removeAll: true
-                },
-                discardUnused: false
-            },
-            canPrint: true
-        })
+      },
+      {
+        // Load all images as base64 encoding if they are smaller than 8192 bytes
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: { name: '[name].[hash:20].[ext]', limit: 8192 }
+          }
+        ]
+      },
+      {
+        // Load all icons
+        test: /\.(eot|woff|woff2|svg|ttf|otf)([\?]?.*)$/,
+        use: [
+          { loader: 'file-loader' }
+        ]
+      },
+      {
+        // HTML LOADER
+        // Reference: https://github.com/webpack/raw-loader
+        // Allow loading html through js
+        test: /\.html$/,
+        loader: 'raw-loader'
+      }
     ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      // Inject the js bundle at the end of the body of the given template
+      inject: 'body',
+    }),
+    // new BaseHrefWebpackPlugin({ baseHref: '/platzieren/' }),
+    new BaseHrefWebpackPlugin({ baseHref: '/' }),
+    new CleanWebpackPlugin(buildPath),
+    new CopyWebpackPlugin({
+      patterns: [{ from: __dirname + '/public' }]
+    }),
+    new MiniCssExtractPlugin({ filename: 'styles.[contenthash].css' }),
+    new OptimizeCssAssetsPlugin({
+      cssProcessor: require('cssnano'),
+      cssProcessorOptions: {
+        map: { inline: false },
+        discardComments: { removeAll: true },
+        discardUnused: false
+      },
+      canPrint: true
+    })
+  ]
 };

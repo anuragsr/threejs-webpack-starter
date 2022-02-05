b42817cdad9906bad93760caf99e4d0988c60501
anuragsr <anurag.131092@gmail.com>
2022-02-05 20:26:44 +0530

    First commit

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -1,6 +1,7 @@
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
 
 const paths = require('./paths')
 
@@ -17,6 +18,9 @@ module.exports = {
 
   // Customize the webpack build process
   plugins: [
+    // Removes FOUC (use instead of style-loader)
+    new MiniCssExtractPlugin(),
+
     // Removes/cleans build folders and unused assets when rebuilding
     new CleanWebpackPlugin(),
 
@@ -37,7 +41,7 @@ module.exports = {
     // Generates an HTML file from a template
     // Generates deprecation warning: https://github.com/jantimon/html-webpack-plugin/issues/1501
     new HtmlWebpackPlugin({
-      title: 'webpack Boilerplate',
+      title: 'Webpack 5 Starter',
       favicon: paths.src + '/images/favicon.png',
       template: paths.src + '/template.html', // template file
       filename: 'index.html', // output file
--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -1,5 +1,5 @@
 const { merge } = require('webpack-merge')
-
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const common = require('./webpack.common')
 
 module.exports = merge(common, {
@@ -24,7 +24,7 @@ module.exports = merge(common, {
       {
         test: /\.(sass|scss|css)$/,
         use: [
-          'style-loader',
+          MiniCssExtractPlugin.loader,
           {
             loader: 'css-loader',
             options: { sourceMap: true, importLoaders: 1, modules: false },
